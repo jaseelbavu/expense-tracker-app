47211e3d1755f17fd304d87759d5b2470e8a7fa6
Jaseel Bavu <jaseelbavu@gmail.com>
2025-07-19 17:18:20 +0400

    Update(HomeTab): Updated home tab with dummy transaction data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(modals)/profileModal" options={{presentation: 'modal'}} />
         <Stack.Screen name="(modals)/walletModal" options={{presentation: 'modal'}} />
+        <Stack.Screen name="(modals)/transactionModal" options={{presentation: 'modal'}} />
       </Stack>
     </AuthProvider>
   );
