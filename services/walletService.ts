5580f552afa8e10238709de98509f570e241d129
Jaseel Bavu <jaseelbavu@gmail.com>
2025-08-03 12:26:52 +0400

    Add(Wallet): Added remove all wallet transaction function while deleting wallet

--- services/walletService.ts
+++ services/walletService.ts
@@ -1,6 +1,6 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, WalletType } from "@/types";
-import { collection, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, setDoc, where, writeBatch } from "firebase/firestore";
 import { uploadFileToCloudinary } from "./imageService";
 
 export const createOrUpdateWallet = async (
@@ -40,10 +40,43 @@ export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
     try {
         const walletRef = doc(firestore, 'wallets', walletId);
         await deleteDoc(walletRef);
+
+        deleteTransactionsByWalletId(walletId);
         
         return {success: true, msg: 'Wallet deleted successfully'}
     } catch(error: any) {
         console.log('Error deleting wallet ', error);
+        return {success: false, msg: error.message};
+    }
+}
+
+export const deleteTransactionsByWalletId = async (walletId: string): Promise<ResponseType> => {
+    try {
+        let hasMoreTransactions = true;
+
+        while(hasMoreTransactions) {
+            const transactionsQuery = query(
+                collection(firestore, 'transactions'),
+                where('walletId', '==', walletId)
+            );
+
+            const transactionsSnapshot = await getDocs(transactionsQuery);
+            if(transactionsSnapshot.size == 0) {
+                hasMoreTransactions = false;
+                break;
+            }
+
+            const batch = writeBatch(firestore);
+            transactionsSnapshot.forEach((transactionDoc) => {
+                batch.delete(transactionDoc.ref);
+            });
+
+            await batch.commit();
+        }
+        
+        return {success: true, msg: 'All the transactions deleted successfully'}
+    } catch(error: any) {
+        console.log('Error deleting wallet transactions ', error);
         return {success: false, msg: error.message};
     }
 }
